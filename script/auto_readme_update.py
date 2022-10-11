67e2aed71c49a8cc517da4595b3c8874832c543b
tony9402 <tony9402@naver.com>
2022-10-11 21:57:04 +0900

    [UPD] README 업데이트

--- script/auto_readme_update.py
+++ script/auto_readme_update.py
@@ -1,20 +1,28 @@
 from glob import glob
 
 
+def convert(text):
+    text = [
+        word.lower().replace(word[0], word[0].upper(), 1)
+        for word in text.split('_')
+    ]
+    return " ".join(text)
+
+
 def make_readme():
-    files = [x[4:-4] for x in glob('src/**/*.cpp', recursive=True)]
+    files = [(x[4:-4], x) for x in glob('src/**/*.cpp', recursive=True)]
     info = dict()
     for file in files:
-        dep = file.split('/')
+        dep = file[0].split('/')
         if dep[0] not in info:
             info[dep[0]] = list()
-        info[dep[0]].append(dep[1])
+        info[dep[0]].append((dep[1], file[1]))
 
     print("# Snippet For VScode\n")
     for key, value in info.items():
-        print(f"## {key}")
-        for x in value:
-            print(f"- {x}")
+        print(f"## {convert(key)}")
+        for x, y in value:
+            print(f"- [{convert(x)}]({y})")
         print()
 
 
