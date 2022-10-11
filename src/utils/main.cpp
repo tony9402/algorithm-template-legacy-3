702abf7d15d5a9e3c27ad5ac7ca81aaa12f97257
tony9402 <tony9402@naver.com>
2022-10-11 21:18:41 +0900

    [ADD] Testcase Main Code 추가

---
+++ src/utils/main.cpp
@@ -0,0 +1,16 @@
+//title: Testcase Basic Main
+//prefix: tcmain, ma
+//description: Main Template for Testcase
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int testcase = $1;$2 cin >> testcase;
+    while(~--testcase) {
+        $3
+    }
+}
