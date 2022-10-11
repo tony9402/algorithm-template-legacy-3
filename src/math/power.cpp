545447aef0d8359b050467d8f5e74373b2bc31f6
tony9402 <tony9402@naver.com>
2022-10-11 21:31:24 +0900

    [ADD] math/power.cpp 추가

---
+++ src/math/power.cpp
@@ -0,0 +1,8 @@
+//title: a^b with modulo
+//prefix: power
+//description: a^b % mod
+template<typename T> T power(T a, T b, T mod) {
+    if(b == 0) return 1;
+    if(b & 1) return a * power(a, b - 1, mod) % mod;
+    return power(a * a % mod, b >> 1, mod);
+}
