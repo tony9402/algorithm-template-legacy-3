22dad748181b932bbc90f6d029e1032fd837fc34
tony9402 <tony9402@naver.com>
2022-10-14 02:50:13 +0900

    [UPD] __int128 삭제

--- src/miscellaneous/main.cpp
+++ src/miscellaneous/main.cpp
@@ -10,13 +10,11 @@ typedef char i8;
 typedef short i16;
 typedef int i32;
 typedef long long i64;
-typedef __int128 i128;
 
 typedef unsigned char u8;
 typedef unsigned short u16;
 typedef unsigned int u32;
 typedef unsigned long long u64;
-typedef unsigned __int128 u128;
 
 typedef float f4;
 typedef double f8;
