0c5326c1f47bf831834b62789b5a8c3570f94a30
tony9402 <tony9402@naver.com>
2022-10-15 23:34:42 +0900

    [ADD] for추가

---
+++ src/miscellaneous/for_[0,N).cpp
@@ -0,0 +1,6 @@
+//title: for [0, N)
+//prefix: f0i, for0n
+//description: for [0, N)
+for(int i = 0; i < ($1); ++i) {
+    $2
+}
---
+++ src/miscellaneous/for_[1,N].cpp
@@ -0,0 +1,6 @@
+//title: for [1, N]
+//prefix: f1i, for1n
+//description: for [1, N]
+for(int i = 1; i <= ($1); ++i) {
+    $2
+}
