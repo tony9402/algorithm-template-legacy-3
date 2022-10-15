5e960d1de14673c0c0c6bebaf2d4b8b147602cde
tony9402 <tony9402@naver.com>
2022-10-15 23:34:15 +0900

    [UPD] modulo default 추가

--- src/math/power.cpp
+++ src/math/power.cpp
@@ -1,7 +1,7 @@
 //title: a^b with modulo
 //prefix: power
 //description: a^b % mod
-template<typename T> T power(T a, T b, T mod) {
+template<typename T> T power(T a, T b, T mod=numeric_limits<T>::max()) {
     if(b == 0) return 1;
     if(b & 1) return a * power(a, b - 1, mod) % mod;
     return power(a * a % mod, b >> 1, mod);
